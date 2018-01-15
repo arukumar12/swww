84ea3507afc5accdb84f306bc5dcaf07433251a3
arukumar12 <30829827+arukumar12@users.noreply.github.com>
2018-01-15 00:33:43 -0800

    Update sd.c

--- .gitignore/sd.c
+++ .gitignore/sd.c
@@ -1,14 +1,15 @@
 
-    #include <stdio.h>
- 
-int main(void)
-{
-    int n,i;
-    printf("Enter the limit");
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
-    	printf("Hello\n");
-    }
-    return 0;
+  #include <stdio.h>
+
+int main() {
+	int i,n,sum=0;
+	printf("enter n.o");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+	    sum=sum+i;
+	   
+	}
+	printf("%d",sum);
+	return 0;
 }
